771d0ef3f8928811c7d4b3827b98857eed3e9513
PJ <95423707+pajamaclaws21@users.noreply.github.com>
2026-03-25 13:51:01 -0500

    update to v1.0

--- main.py
+++ main.py
@@ -8,20 +8,13 @@ from flask_cors import CORS, cross_origin
 
 app = flask.Flask(__name__)
 uaProduct = "WBIB4"
-uaVersion = "0.9"
+uaVersion = "1.0"
 
 # if you are forking: CHANGE THESE!!
 allowedOrigins = ["https://pajamaclaws.net", "https://www.pajamaclaws.net"]
 userAgent = "pajamaclaws-net"
 
 CORS(app, origins=allowedOrigins)
-
-def randomRequestID():
-    id = "a"
-    chars = list("qwertyuiopasdfghjklzxcvbnm1234567890")
-    for n in range(0, 25):
-        id += chars[random.randrange(0, len(chars))]
-    return id
 
 def formatUrl(url):
     if "://" not in url or "http" not in url:
@@ -42,8 +35,6 @@ def unformUrl(url):
     if url[len(url) - 1] != "/":
         url += "/"
     return url
-    
-currentRequests = []
 
 @app.route("/")
 def index():
@@ -53,87 +44,74 @@ def index():
 def howto():
     return app.send_static_file(filename="howto.html")
 
-@app.route("/api/id")
-def deliverId():
-    if not flask.request.headers.get('origin') in allowedOrigins:
-        flask.abort(401)
-    thisRequestID = randomRequestID()
-    currentRequests.append(thisRequestID)
-    return thisRequestID
-
 # base from https://browser.engineering/http.html
-@app.route("/api/access/<string:url>/<string:id>")
-def access(url, id, redirectNum=0):
+@app.route("/api/access/<string:url>")
+def access(url, , redirectNum=0):
     if not flask.request.headers.get('origin') in allowedOrigins:
         flask.abort(401)
-    if id in currentRequests:
-        # if we've followed more than five redirects, give up
-        if redirectNum > 5:
-            return "Gave up following redirects"
+
+    # if we've followed more than five redirects, give up
+    if redirectNum > 5:
+        return "Gave up following redirects"
         
-        url = urllib.parse.urlsplit(unformUrl(url))  # get the parts of the url
+    url = urllib.parse.urlsplit(unformUrl(url))  # get the parts of the url
+    request = "\r\n".join([f"GET {url.path} HTTP/1.0", f"User-Agent: {uaProduct}/{uaVersion} {userAgent}", "Connection: close", "X-Powered-By: wbib4", f"Host: {url.hostname}", "\r\n"])
 
-        request = "\r\n".join([f"GET {url.path} HTTP/1.0", f"User-Agent: {uaProduct}/{uaVersion} {userAgent}", "Connection: close", "X-Powered-By: wbib4", f"Host: {url.hostname}", "\r\n"])
-
-        s = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM, proto=socket.IPPROTO_TCP)  # open socket
+    s = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM, proto=socket.IPPROTO_TCP)  # open socket
+    # decide what port to listen to
+    if url.scheme == "http":
+        port = 80
+    elif url.scheme == "https":
+        port = 443
     
-        # decide what port to listen to
-        if url.scheme == "http":
-            port = 80
-        elif url.scheme == "https":
-            port = 443
-
-        # if the url has a port in it, decide that port instead
-        if url.port != None:
-            port = url.port
+    # if the url has a port in it, decide that port instead
+    if url.port != None:
+        port = url.port
     
-        # open the socket
-        s.connect((url.hostname, port))
-        # if it's https, use ssl
-        if url.scheme == "https":
-            ctx = ssl.create_default_context()
-            s = ctx.wrap_socket(s, server_hostname=url.hostname)
-            # build the request & send it
-        s.send(request.encode("utf8"))
-        # get our response & close the socket
-        response = s.makefile("r", encoding="utf8", newline="\r\n")
-        content = response.read()
-        s.close()
+    # open the socket
+    s.connect((url.hostname, port))
+    # if it's https, use ssl
+    if url.scheme == "https":
+        ctx = ssl.create_default_context()
+        s = ctx.wrap_socket(s, server_hostname=url.hostname)
+    # build the request & send it
+    s.send(request.encode("utf8"))
+    # get our response & close the socket
+    response = s.makefile("r", encoding="utf8", newline="\r\n")
+    content = response.read()
+    s.close()
 
+    # allows us to better understand content
+    content = content.split("\r\n")
 
-        # allows us to better understand content
-        content = content.split("\r\n")
+    httpDict = {}
+    finalReturn = []
 
-        httpDict = {}
-        finalReturn = []
+    for item in content:
+        index = content.index(item)
+        if ": " in item and index != len(content) - 1:
+            # adds response data to a dictionary
+            item = item.split(": ")
+            key = item[0]
+            value = item[1]
+            httpDict[key] = value
+        elif "HTTP/" in item:
+            # adds data about response to output: HTTP version, status code, status name
+            finalReturn.append([item[:8], item.split(" ")[1], item[13:]])
+        elif item != "":
+            # add all non-empty items to final value
+            finalReturn.append(item)
 
-        for item in content:
-            index = content.index(item)
-            if ": " in item and index != len(content) - 1:
-                # adds response data to a dictionary
-                item = item.split(": ")
-                key = item[0]
-                value = item[1]
-                httpDict[key] = value
-            elif "HTTP/" in item:
-                # adds data about response to output: HTTP version, status code, status name
-                finalReturn.append([item[:8], item.split(" ")[1], item[13:]])
-            elif item != "":
-                # add all non-empty items to final value
-                finalReturn.append(item)
-
-        # makes setup of list as follows: HTTP version/code/name, response data, response body
-        finalReturn.insert(1, httpDict)
+    # makes setup of list as follows: HTTP version/code/name, response data, response body
+    finalReturn.insert(1, httpDict)
         
-        # checks that the status code is not a redirect
-        if finalReturn[0][1][0] == "3":
-            try:
-                location = httpDict["location"]
-            except:
-                location = httpDict["Location"]
+    # checks that the status code is not a redirect
+    if finalReturn[0][1][0] == "3":
+        try:
+            location = httpDict["location"]
+        except:
+            location = httpDict["Location"]
 
-            return access(formatUrl(location), id, redirectNum+1)
-        currentRequests.remove(id)
-        return finalReturn
-    else:
-        flask.abort(401)
+        return access(formatUrl(location), id, redirectNum+1)
+    
+    return finalReturn
